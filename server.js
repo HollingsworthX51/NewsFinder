fc1ab5ec53c98629298e02dee45e31038817eae8
hworthX51@hotmail.com <you@example.com>
2018-02-03 19:22:21 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+var cheerio = require('cheerio');
+var request = require('request');
+var express = require("express");
+var handlebars = require('express-handlebars');
+var mongoose = require('mongoose');
+var mongojs = require("mongojs");
+var bodyParser = require('body-parser');
